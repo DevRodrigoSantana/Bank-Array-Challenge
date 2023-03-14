1ecf8b6333e2130b2edd05c2d61705b85a0bdf30
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2023-03-14 15:09:43 -0300

    construtores de Cliente

---
+++ Banco/src/Cliente.java
@@ -0,0 +1,32 @@
+
+public class Cliente {
+    
+        // variaveis para guardar nome, numero da conta e saldo do cliente 
+        
+        private  int numeroConta;
+        private String nomeCliente;
+        private float saldo;
+        
+        //get's das vaiaveis. Cada  pega o valor setado e armazena
+        public int getNumeroConta() {
+            return numeroConta;
+        }
+        public void setNumeroConta(int numeroConta) {
+            this.numeroConta = numeroConta;
+        }
+        public String getNomeCliente() {
+            return nomeCliente;
+        }
+        public void setNomeCliente(String nomeCliente) {
+            this.nomeCliente = nomeCliente;
+        }
+        public float getSaldo() {
+            return saldo;
+        }
+        public void setSaldo(float saldo) {
+            this.saldo = saldo;
+        }
+        
+        
+        
+    }
